4d48d4bd3af2a4ced3557f34aaf47d255e39e3a2
Home <home@Samanis-MacBook-Pro.local>
2019-03-18 22:16:19 +0000

    Setup & API call

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const calculate = () => {
+    const accountSize = document.getElementsByName('accountsize')[0];
+    const pips = document.getElementsByName('pips')[0];
+    const currency1 = document.getElementsByName('currency1')[0].value.toUpperCase();
+    const currency2 = document.getElementsByName('currency2')[0].value.toUpperCase();
+    const riskPercentage = document.getElementsByName('riskpercentage')[0];
+
+    const baseURL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE`;
+    const symbol = `from_currency=${currency1}&to_currency=${currency2}`;
+    const apikey = `apikey=${API_KEY}`;
+    
+    axios
+    .get(`${baseURL}&${symbol}&${apikey}`)
+    .then(({ data }) => console.log('data: ', data['Realtime Currency Exchange Rate']))
+    .catch(e => console.log('e: ', e))
+}
