b9c884a56d450cb5956f2c2663557a04a0eb6c55
Samanimkr <Samanimkr@outlook.com>
2019-03-18 22:29:33 +0000

    labels

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-
+const API_KEY = 'K8SVFGG987OJSE6W';
 
 const calculate = () => {
-    const accountSize = document.getElementsByName('accountsize')[0];
-    const pips = document.getElementsByName('pips')[0];
+    const accountSize = document.getElementsByName('accountsize')[0].value;
+    const pips = document.getElementsByName('pips')[0].value;
     const currency1 = document.getElementsByName('currency1')[0].value.toUpperCase();
     const currency2 = document.getElementsByName('currency2')[0].value.toUpperCase();
-    const riskPercentage = document.getElementsByName('riskpercentage')[0];
+    const riskPercentage = document.getElementsByName('riskpercentage')[0].value;
 
     const baseURL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE`;
     const symbol = `from_currency=${currency1}&to_currency=${currency2}`;
@@ -13,6 +13,9 @@ const calculate = () => {
     
     axios
     .get(`${baseURL}&${symbol}&${apikey}`)
-    .then(({ data }) => console.log('data: ', data['Realtime Currency Exchange Rate']))
+    .then(({ data }) => {
+        console.log('data: ', data['Realtime Currency Exchange Rate']);
+        
+    })
     .catch(e => console.log('e: ', e))
 }
